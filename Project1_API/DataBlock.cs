a37054a39308d4e60ff1bfc3b3f740601a164513
MikhailArtemyev <136899915+MikhailArtemyev@users.noreply.github.com>
2023-06-17 13:25:20 +0100

    first commit
    API project

---
+++ Project1_API/DataBlock.cs
@@ -0,0 +1,16 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class DataBlock
+{
+    public DateTime Date { get; set; }
+    public int MinTemp { get; set; }
+    public int MaxTemp { get; set; }
+
+    public DataBlock(DateTime a, int b, int c)
+    {
+        Date = a;
+        MinTemp = b;
+        MaxTemp = c;
+    }
+        
+}
